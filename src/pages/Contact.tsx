18733576f350a47bcda80b989f3d45a47e24d39a
Venu Mahajan <mahajanvenu57@gmail.com>
2025-10-23 12:50:02 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -260,11 +260,11 @@ const Contact = () => {
             <div className="rounded-2xl overflow-hidden shadow-elegant">
               <iframe
                 src="<iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9344965548767!2d72.81061727498636!3d19.415235981860413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7a9004bb04f61%3A0x281e3ba5de838dcf!2sPoonam%20Palace!5e0!3m2!1sen!2sin!4v1761203567783!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9344965548767!2d72.81061727498636!3d19.415235981860413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7a9004bb04f61%3A0x281e3ba5de838dcf!2sPoonam%20Palace!5e0!3m2!1sen!2sin!4v1761203567783!5m2!1sen!2sin"
                 width="100%"
                 height="300"
                 style={{ border: 0 }}
-                allowFullScreen
+                allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
                 title="Hexamad Digital Location - Poonam Palace, B-301, near Star Hospital, Nalasopara West"
